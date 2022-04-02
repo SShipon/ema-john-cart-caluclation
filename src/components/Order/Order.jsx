fd0a59d4f84026223eef11df790a9f033975564a
SShipon <nxshipon@gmail.com>
2022-04-02 14:39:35 +0630

    hey dear

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart.jsx';
 import useProduct from '../../hooks/useProducts.jsx';
 import { removeFromDb } from '../../utilities/fakedb.js';
+import './Order.css'
 import Cart from '../Cart/Cart.js';
 import ReviewItem from '../ReviewItem/ReviewItem.jsx';
 
@@ -16,18 +18,22 @@ const Order = () => {
     }
     return (
       <div className="shop-container">
-            <div className="reviewItem-container">
-                {
-                    cart.map(product => <ReviewItem
-                        key={product.id}
-                        product={product}
-                        handelRemoveProduct={handelRemoveProduct}
-                    ></ReviewItem>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
+        <div className="reviewItem-container">
+          {cart.map((product) => (
+            <ReviewItem
+              key={product.id}
+              product={product}
+              handelRemoveProduct={handelRemoveProduct}
+            ></ReviewItem>
+          ))}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart}>
+            <Link to="/inventory">
+                <button>Proceed checkout </button>
+            </Link>
+          </Cart>
+        </div>
       </div>
     );
 };
